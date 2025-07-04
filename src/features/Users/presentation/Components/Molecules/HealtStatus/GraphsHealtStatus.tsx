5b2c2e62e632c965cbb22854948115ac25372b8d
Regiber18 <regiber.montoya2005@hotmail.com>
2025-07-04 01:25:11 -0600

    complete graphic data, update socket provider and create humidity graph

--- src/features/Users/presentation/Components/Molecules/HealtStatus/GraphsHealtStatus.tsx
+++ src/features/Users/presentation/Components/Molecules/HealtStatus/GraphsHealtStatus.tsx
@@ -1,36 +1,52 @@
 import BarChartHealtStatus from "./BarChartHealtStatus";
 import LineChartHealtStatus from "./LineChartHealtStatus";
-import style from "../HealtStatus/Healt.module.css"
+import style from "../HealtStatus/Healt.module.css";
 import { useWebSocket } from "../../../../../../shared/HumidityProvider";
 
 function GraphsHealtStatus() {
-    const { messages } = useWebSocket();
+  const { temperatureData, humidityData } = useWebSocket();
 
-    const glucoseData = {
-        labels: ["Semana 1", "Semana 2", "Semana 3", "Semana 4"],
-        values: messages,
-    };
+  const labels = temperatureData?.intervalos || [];
+  const marcas = temperatureData?.marcas || [];
 
-    const symptomsData = {
-        labels: ["Dolor", "Fatiga", "Náuseas"],
-        values: [3, 5, 2],
-    };
-    return ( 
-        <>
-        <div className="mt-10">
-            <p className="text-2xl text-[#00000081] ml-7">Evolución de mi salud</p>
+  const glucoseData = {
+    labels: labels,
+    values: marcas,
+  };
+
+  const symptomsData = {
+    labels: ["Dolor", "Fatiga", "Náuseas"],
+    values: [3, 5, 2],
+  };
+
+  return (
+    <>
+      <div className="mt-10">
+        <p className="text-2xl text-[#00000081] ml-7">Evolución de mi salud</p>
+      </div>
+      <div className="flex sm:flex-row flex-wrap">
+        <div
+          className="w-[50vh] h-[40vh] sm:w-[80vh] sm:h-[40vh]"
+          id={style.lineChart}
+        >
+          <LineChartHealtStatus
+            labels={glucoseData.labels}
+            dataValues={glucoseData.values}
+          />
         </div>
-        <div className="flex sm:flex-row flex-wrap">
-            <div className="w-[50vh] h-[40vh] sm:w-[80vh] sm:h-[40vh]" id={style.lineChart}>
-            <LineChartHealtStatus labels={glucoseData.labels} dataValues={glucoseData.values}></LineChartHealtStatus>
-            </div>
 
-            <div className="w-[50vh] h-[40vh] sm:w-[80vh] sm:h-[40vh]" id={style.BarChart}>
-            <BarChartHealtStatus labels={symptomsData.labels} dataValues={glucoseData.values}></BarChartHealtStatus>
-            </div>
+        <div
+          className="w-[50vh] h-[40vh] sm:w-[80vh] sm:h-[40vh]"
+          id={style.BarChart}
+        >
+          <BarChartHealtStatus
+            labels={glucoseData.labels}
+            dataValues={glucoseData.values}
+          />
         </div>
-        </>
-     );
+      </div>
+    </>
+  );
 }
 
 export default GraphsHealtStatus;
--- src/features/Users/presentation/Components/Organism/PatientsRegistersOrganism/PatientsRegisters.tsx
+++ src/features/Users/presentation/Components/Organism/PatientsRegistersOrganism/PatientsRegisters.tsx
@@ -1,19 +1,38 @@
 import GraphTemperature from "../../Molecules/PacientesRegistrados/graphTemperature";
 import InventoryCards from "../../Molecules/PacientesRegistrados/InventoryCards";
 import TablePatientsRegister from "../../Molecules/PacientesRegistrados/tablePatientsRegisters";
+import { useWebSocket } from "../../../../../../shared/HumidityProvider";
+import GraphHumidity from "../../Molecules/PacientesRegistrados/GraphHumidity";
 
-const labels = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
-const temperatures = [23, 26, 25, 28, 24];
 function PatientsRegisters() {
-    return ( 
+    const { temperatureData, humidityData } = useWebSocket();
+
+    const labels = temperatureData?.intervalos || [];
+    const marcas = temperatureData?.marcas || [];
+
+    const temperature = {
+        labels: labels,
+        values: marcas,
+    };
+
+    const labelsH = humidityData?.intervalos || [];
+    const valuesH = humidityData?.intervalos || []
+
+
+    const humidity = {
+        labels: labelsH,
+        values: valuesH
+    }
+    return (
         <>
             <TablePatientsRegister></TablePatientsRegister>
             <div className="flex-none mt-3 gap-6 sm:flex">
-            <GraphTemperature labels={labels} temperatures={temperatures}></GraphTemperature>
-            <InventoryCards></InventoryCards>
+                <GraphTemperature labels={temperature.labels} temperatures={temperature.values}></GraphTemperature>
+                <GraphHumidity labels={humidity.labels} humidity={humidity.values} ></GraphHumidity>
+                <InventoryCards></InventoryCards>
             </div>
         </>
-     );
+    );
 }
 
 export default PatientsRegisters;
